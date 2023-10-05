a914017e38b9734a68a7db00373a330c50d1ea3f
Rocío Cejudo <rocio.cejudo.soronellas@gmail.com>
2023-10-05 15:55:09 +0200

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+// al ser un componente que se rederiza desde el servidor, necesitamos que el app router de nextjs cargue la informacion que necesitamos
+import { cookies } from "next/headers"
+
+export default async function Home() {
+  const supabase = createServerComponentClient({cookies})
+
+  const {data:posts} = await supabase.from('posts').select('*')
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     Hola twitter
+     <pre>
+      {JSON.stringify(posts, null, 2)}
+     </pre>
+    </main>
+  )
+}
